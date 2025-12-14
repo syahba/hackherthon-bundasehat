087fdcdc1969d12cc08e4e108bb8161ceb6ba339
syahba <khalisasyahba@gmail.com>
2025-12-14 22:45:28 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import MindMapPage from "./pages/MindMapPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/hackherthon-bundasehat/">
       <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route
